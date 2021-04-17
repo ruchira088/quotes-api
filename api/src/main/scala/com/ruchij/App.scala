a55437bbd448752219979525ca8512820b7a37eb
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-04-17 23:22:19 +1000

    Wiring up the HttpApp

--- api/src/main/scala/com/ruchij/App.scala
+++ api/src/main/scala/com/ruchij/App.scala
@@ -1,9 +1,15 @@
 package com.ruchij
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{Async, Clock, ContextShift, ExitCode, IO, IOApp, Resource}
 import com.ruchij.config.ServiceConfiguration
+import com.ruchij.dao.doobie.DoobieTransactor
+import com.ruchij.dao.quote.DoobieQuoteDao
 import com.ruchij.services.health.HealthServiceImpl
+import com.ruchij.services.quote.QuotationServiceImpl
+import com.ruchij.types.Random.randomUuid
 import com.ruchij.web.Routes
+import doobie.ConnectionIO
+import org.http4s.HttpApp
 import org.http4s.server.blaze.BlazeServerBuilder
 import pureconfig.ConfigSource
 
@@ -15,14 +21,25 @@ object App extends IOApp {
       configObjectSource <- IO.delay(ConfigSource.defaultApplication)
       serviceConfiguration <- ServiceConfiguration.parse[IO](configObjectSource)
 
-      healthService = new HealthServiceImpl[IO](serviceConfiguration.buildInformation)
-
       _ <-
-        BlazeServerBuilder.apply[IO](ExecutionContext.global)
-          .withHttpApp(Routes(healthService))
-          .bindHttp(serviceConfiguration.httpConfiguration.port, serviceConfiguration.httpConfiguration.host)
-          .serve.compile.drain
-
+        program[IO](serviceConfiguration)
+          .use {
+            httpApp =>
+              BlazeServerBuilder.apply[IO](ExecutionContext.global)
+                .withHttpApp(httpApp)
+                .bindHttp(serviceConfiguration.httpConfiguration.port, serviceConfiguration.httpConfiguration.host)
+                .serve.compile.drain
+          }
     }
     yield ExitCode.Success
+
+  def program[F[+ _]: Async: ContextShift: Clock](serviceConfiguration: ServiceConfiguration): Resource[F, HttpApp[F]] =
+    DoobieTransactor.create(serviceConfiguration.doobieDatabaseConfiguration)
+      .map(_.trans)
+      .map { implicit transactor =>
+        val quotationService = new QuotationServiceImpl[F, ConnectionIO](DoobieQuoteDao)
+        val healthService = new HealthServiceImpl[F](serviceConfiguration.buildInformation)
+
+        Routes(quotationService, healthService)
+      }
 }
--- api/src/main/scala/com/ruchij/web/Routes.scala
+++ api/src/main/scala/com/ruchij/web/Routes.scala
@@ -2,19 +2,21 @@ package com.ruchij.web
 
 import cats.effect.Sync
 import com.ruchij.services.health.HealthService
+import com.ruchij.services.quote.QuotationService
 import com.ruchij.web.middleware.{ExceptionHandler, NotFoundHandler}
-import com.ruchij.web.routes.HealthRoutes
+import com.ruchij.web.routes.{HealthRoutes, QuoteRoutes}
 import org.http4s.{HttpApp, HttpRoutes}
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 
 object Routes {
-  def apply[F[_]: Sync](healthService: HealthService[F]): HttpApp[F] = {
+  def apply[F[_]: Sync](quotationService: QuotationService[F], healthService: HealthService[F]): HttpApp[F] = {
     implicit val dsl: Http4sDsl[F] = new Http4sDsl[F] {}
 
     val routes: HttpRoutes[F] =
       Router(
-        "/service" -> HealthRoutes(healthService)
+        "/service" -> HealthRoutes(healthService),
+        "/quote" -> QuoteRoutes(quotationService)
       )
 
     ExceptionHandler {
